7e7d70219b88452ccc60375bdd91f73db4cd520e
keni <14bass@kk.ri>
2025-12-31 12:48:39 +0300

    update yaml

--- tests/test_button.py
+++ tests/test_button.py
@@ -1,6 +1,11 @@
 from conftest import driver
 
 
-def test_button_exist(driver):
+def test_button_1_exist(driver):
     driver.get("https://www.qa-practice.com/elements/button/simple")
     assert driver.find_element("xpath", "//input[@id='submit-id-submit']").is_displayed()
+
+
+def test_button_2_exist(driver):
+    driver.get("https://www.qa-practice.com/elements/button/like_a_button")
+    assert driver.find_element("xpath", "//a[text()='Click']").is_displayed()
